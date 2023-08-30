d355e08bb704375c73279b1a35a5b3339b0f6643
Franco Abaroa <franco9395@gmail.com>
2023-08-30 12:53:52 -0400

    Implement multi-node and fault tolerant broadcast

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -2,55 +2,181 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"sync"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-// Define a global variable to store the messages
-var messages []int
+type NodeState struct {
+	node     *maelstrom.Node
+	id       string
+	messages []int
+	peers    []string
+	mu       sync.Mutex
+}
+
+type BroadcastRequestBody struct {
+	Type    string `json:"type"`
+	Message int    `json:"message"`
+}
+
+type BroadcastResponseBody struct {
+	Type string `json:"type"`
+}
+
+type BroadcastOkResponseBody struct {
+	InReplyTo int    `json:"in_reply_to"`
+	Type      string `json:"type"`
+}
+
+func containsInt(slice []int, item int) bool {
+	for _, a := range slice {
+		if a == item {
+			return true
+		}
+	}
+	return false
+}
+
+func containsStr(slice []string, item string) bool {
+	for _, a := range slice {
+		if a == item {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
 	n := maelstrom.NewNode()
+	state := &NodeState{node: n}
 
-	// Handle "broadcast"
-	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		var body map[string]any
+	// Handle "init"
+	n.Handle("init", func(msg maelstrom.Message) error {
+		var body struct {
+			Type    string   `json:"type"`
+			MsgID   int      `json:"msg_id"`
+			NodeID  string   `json:"node_id"`
+			NodeIDs []string `json:"node_ids"`
+		}
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		// Store the message
-		messages = append(messages, int(body["message"].(float64)))
+		state.mu.Lock()
+		state.id = body.NodeID
+		state.mu.Unlock()
 
-		// Delete message from response
-		delete(body, "message")
+		reply := map[string]interface{}{
+			"type":        "init_ok",
+			"in_reply_to": body.MsgID,
+		}
+		return n.Reply(msg, reply)
+	})
 
-		// Return a broadcast_ok message
-		body["type"] = "broadcast_ok"
-		return n.Reply(msg, body)
+	// Handle "broadcast"
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body BroadcastRequestBody
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		state.mu.Lock()
+		defer state.mu.Unlock()
+
+		// Check if we have already seen this message
+		if containsInt(state.messages, body.Message) {
+			reply := BroadcastResponseBody{
+				Type: "broadcast_ok",
+			}
+			return n.Reply(msg, reply)
+		}
+
+		// Store the new message
+		state.messages = append(state.messages, body.Message)
+
+		go func(destinations []string, body BroadcastRequestBody) {
+			var successfulSentDestinations []string
+			var successfulSentDestinationsMutex sync.Mutex
+
+			for len(destinations) != len(successfulSentDestinations) {
+				for _, node := range destinations {
+					if !containsStr(successfulSentDestinations, node) {
+						n.RPC(node, body, func(msg maelstrom.Message) error {
+							var broadcastOkBody BroadcastOkResponseBody
+
+							if err := json.Unmarshal(msg.Body, &broadcastOkBody); err != nil {
+								return err
+							}
+
+							if broadcastOkBody.Type != "broadcast_ok" {
+								return fmt.Errorf("expected type broadcast_ok, got %s", broadcastOkBody.Type)
+							}
+
+							successfulSentDestinationsMutex.Lock()
+							successfulSentDestinations = append(successfulSentDestinations, node)
+							successfulSentDestinationsMutex.Unlock()
+
+							return nil
+						})
+					}
+				}
+				time.Sleep(1 * time.Second)
+			}
+		}(state.peers, body)
+
+		reply := BroadcastResponseBody{
+			Type: "broadcast_ok",
+		}
+		return n.Reply(msg, reply)
 	})
 
 	// Handle "read"
 	n.Handle("read", func(msg maelstrom.Message) error {
-		// Send back all the messages we've received
-		body := map[string]any{
-			"type":     "read_ok",
-			"messages": messages,
+		var body struct {
+			Type  string `json:"type"`
+			MsgID int    `json:"msg_id"`
 		}
-		return n.Reply(msg, body)
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		state.mu.Lock()
+		defer state.mu.Unlock()
+
+		reply := map[string]interface{}{
+			"type":        "read_ok",
+			"messages":    state.messages,
+			"in_reply_to": body.MsgID,
+		}
+		return n.Reply(msg, reply)
 	})
 
 	// Handle "topology"
 	n.Handle("topology", func(msg maelstrom.Message) error {
-		// For now, we just acknowledge receipt of the topology
-		body := map[string]any{
-			"type": "topology_ok",
+		var body struct {
+			Type     string              `json:"type"`
+			Topology map[string][]string `json:"topology"`
+			MsgID    int                 `json:"msg_id"`
 		}
-		return n.Reply(msg, body)
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		state.mu.Lock()
+		defer state.mu.Unlock()
+
+		state.peers = body.Topology[state.id]
+
+		reply := map[string]interface{}{
+			"type":        "topology_ok",
+			"in_reply_to": body.MsgID,
+		}
+		return n.Reply(msg, reply)
 	})
 
-	if err := n.Run(); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(n.Run())
 }
